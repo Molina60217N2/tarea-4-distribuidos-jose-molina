e98f4dd62e1815023288186e9ba21423a0497b74
Molina60217N2 <molinasalas16@gmail.com>
2024-10-21 13:39:22 -0600

    saf

--- netlify/functions/authorUpdate.js
+++ netlify/functions/authorUpdate.js
@@ -1,35 +1,62 @@
-"use strict";
+// "use strict";
 
-const { ObjectId } = require('mongodb');
-const clientPromise = require('./mongoDB');
+// const { ObjectId } = require('mongodb');
+// const clientPromise = require('./mongoDB');
+// const headers = require('./headersCORS');
+
+// exports.handler = async (event, context) => {
+
+//   if (event.httpMethod == "OPTIONS") {
+//     return { statusCode: 200, headers, body: "OK" };
+//   }
+
+//   try {
+//     const client = await clientPromise;
+//     const id = event.path.split("/").reverse()[0];  // Obtener el ID del path
+//     const data = JSON.parse(event.body);  // Datos que se enviarán para actualizar
+//     console.log(data);
+
+//     // Excluir _id del objeto de actualización si está presente
+//     delete data._id;
+
+//     // Actualizar el documento utilizando _id como filtro
+//     const result = await client.db("books").collection("authors").updateOne(
+//       { _id: new ObjectId(id) }, // Buscar por ObjectId
+//       { $set: data }  // Actualizar con los datos recibidos
+//     );
+
+//     if (result.matchedCount === 0) {
+//       return { statusCode: 404, headers, body: 'Book not found' };
+//     }
+
+//     return { statusCode: 200, headers, body: 'Book updated successfully' };
+//   } catch (error) {
+//     console.log(error);
+//     return { statusCode: 422, headers, body: JSON.stringify(error) };
+//   }
+// };
+
+
+"use strict"
+
 const headers = require('./headersCORS');
+
+const rabbitPromise = require('./rabbitMQ');
 
 exports.handler = async (event, context) => {
 
   if (event.httpMethod == "OPTIONS") {
-    return { statusCode: 200, headers, body: "OK" };
+    return {statusCode: 200,headers,body: "OK"};
   }
 
   try {
-    const client = await clientPromise;
-    const id = event.path.split("/").reverse()[0];  // Obtener el ID del path
-    const data = JSON.parse(event.body);  // Datos que se enviarán para actualizar
-    console.log(data);
+    const id = event.path.split("/").reverse()[0];
 
-    // Excluir _id del objeto de actualización si está presente
-    delete data._id;
+    const channel = await rabbitPromise();
+    const request = `{'method':'UPDATE','id':${id},'body':${event.body}}`;
+    await channel.sendToQueue("bookstore", Buffer.from(request));
 
-    // Actualizar el documento utilizando _id como filtro
-    const result = await client.db("books").collection("authors").updateOne(
-      { _id: new ObjectId(id) }, // Buscar por ObjectId
-      { $set: data }  // Actualizar con los datos recibidos
-    );
-
-    if (result.matchedCount === 0) {
-      return { statusCode: 404, headers, body: 'Book not found' };
-    }
-
-    return { statusCode: 200, headers, body: 'Book updated successfully' };
+    return { statusCode: 200, headers, body: 'OK'};
   } catch (error) {
     console.log(error);
     return { statusCode: 422, headers, body: JSON.stringify(error) };
--- netlify/functions/publisherDelete.js
+++ netlify/functions/publisherDelete.js
@@ -1,30 +1,57 @@
-"use strict";
+// "use strict";
 
-const { ObjectId } = require('mongodb');  // Importar ObjectId
-const clientPromise = require('./mongoDB');
+// const { ObjectId } = require('mongodb');  // Importar ObjectId
+// const clientPromise = require('./mongoDB');
+// const headers = require('./headersCORS');
+
+// exports.handler = async (event, context) => {
+
+//   if (event.httpMethod == "OPTIONS") {
+//     return { statusCode: 200, headers, body: "OK" };
+//   }
+
+//   try {
+//     const client = await clientPromise;
+//     const id = event.path.split("/").reverse()[0];  // Obtener el ID del path
+//     console.log(id);
+
+//     // Convertir el id en ObjectId para la consulta de eliminación
+//     const result = await client.db("books").collection("publishers").deleteOne({ _id: new ObjectId(id) });
+
+//     if (result.deletedCount === 0) {
+//       return { statusCode: 404, headers, body: 'Book not found' };
+//     }
+
+//     return { statusCode: 200, headers, body: 'Book deleted successfully' };
+//   } catch (error) {
+//     console.log(error);
+//     return { statusCode: 422, headers, body: JSON.stringify(error) };
+//   }
+// };
+
+
+"use strict"
+
 const headers = require('./headersCORS');
+
+const rabbitPromise = require('./rabbitMQ');
 
 exports.handler = async (event, context) => {
 
   if (event.httpMethod == "OPTIONS") {
-    return { statusCode: 200, headers, body: "OK" };
+    return {statusCode: 200,headers,body: "OK"};
   }
 
   try {
-    const client = await clientPromise;
-    const id = event.path.split("/").reverse()[0];  // Obtener el ID del path
-    console.log(id);
+    const id = event.path.split("/").reverse()[0];
 
-    // Convertir el id en ObjectId para la consulta de eliminación
-    const result = await client.db("books").collection("publishers").deleteOne({ _id: new ObjectId(id) });
+    const channel = await rabbitPromise();
+    const request = `{'method':'DELETE','id': ${id} }`;
+    await channel.sendToQueue("bookstore", Buffer.from(request));
 
-    if (result.deletedCount === 0) {
-      return { statusCode: 404, headers, body: 'Book not found' };
-    }
-
-    return { statusCode: 200, headers, body: 'Book deleted successfully' };
+    return {statusCode: 200,headers,body: status};
   } catch (error) {
     console.log(error);
-    return { statusCode: 422, headers, body: JSON.stringify(error) };
+    return {statusCode: 422,headers,body: JSON.stringify(error)};
   }
 };
